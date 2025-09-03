0efbae4b69b7544922e0f8320b3e466e5b7ecaa7
WaqarHassan20 <waqarkhalid2024@gmail.com>
2025-09-04 00:11:51 +0500

    Kafka basics

--- 03_Kafka/consumer.ts
+++ 03_Kafka/consumer.ts
@@ -1,19 +1,21 @@
 import { kafkaClient } from "./client";
+const group = process.argv[2] || "group-1";
 
 async function init() {
-  const consumer = kafkaClient.consumer({ groupId: "group-1" });
+  const consumer = kafkaClient.consumer({ groupId: group });
 
-  console.log("Consumer connecting...");
+  console.log(`Consumer [${group}] connecting...`);
   await consumer.connect();
-  console.log("Consumer connected Done!");
+  console.log(`Consumer [${group}] connected`);
 
   await consumer.subscribe({ topic: "rider-updates", fromBeginning: true });
-  console.log('Consumer subscribed to topic "rider-updates"');
+  console.log(`Consumer [${group}] subscribed to "rider-updates"`);
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       console.log(
-        `📩 ${topic} | Partition: ${partition} | Message: ${message.value?.toString()}`
+        `[${group}] ${topic} | PART:${partition} |`,
+        message.value?.toString()
       );
     },
   });
